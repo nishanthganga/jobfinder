5bbf6d68711cdbafc2bc3594f1daca19aee3a08d
Nishanth G <developer.nishanth@gmail.com>
2017-10-26 20:11:29 +0530

    Trys out connecting to Twitter and fetch tweets

--- lib/data_from_external_source.rb
+++ lib/data_from_external_source.rb
@@ -7,10 +7,19 @@ module DataFromExternalSource
       config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
     end
 
-    client.search(search_term, result_type: 'recent').take(6).collect do |tweet|
-       Rails.logger.info(tweet.inspect)
-      "#{tweet.user.screen_name}: #{tweet.text}"
+    since_id = nil #get the id of the last record 
+    @recent_tweets = []
+    client.search(search_term, result_type: 'recent', since_id: since_id).take(6).collect do |tweet|
+       @recent_tweets << tweet
     end
+
+    return @recent_tweets
+
   end
+
+
 end
 
+
+# [#<Twitter::Tweet id=923450898478645248>, #<Twitter::Tweet id=923430523732606977>, #<Twitter::Tweet id=923430522491101184>, #<Twitter::Tweet id=923430520972746753>, #<Twitter::Tweet id=923430519823409153>, #<Twitter::Tweet id=923430518560919552>]
+# [#<Twitter::Tweet id=923450898478645248>, #<Twitter::Tweet id=923430523732606977>]
